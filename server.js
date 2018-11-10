4e23a8d14d230608c30522a145169039178a8b84
Jeme <aime.urquieta@outlook.com>
2018-11-10 12:49:48 -0600

    fixing heroku errors

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 const mysql=require('mysql');
 const path=require('path');
 
-const connection=mysql.createConnection({
-    port:process.env.DB_PORT||3000,
-    host:process.env.DB_HOST||`localhost`,
-    password:process.env.DB_PASSWORD||`root`,
-    database:process.env.DB_DATABASE||`dataFriends_bd`,
-    user:process.env.DB_USER||`root`,
-})
+//const connection=mysql.createConnection({
+ //   port:process.env.DB_PORT||8080,
+  //  host:process.env.DB_HOST||`localhost`,
+  //  password:process.env.DB_PASSWORD||`root`,
+  //  database:process.env.DB_DATABASE||`dataFriends_bd`,
+  //  user:process.env.DB_USER||`root`,
+//})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +18,6 @@ app.use(express.static(path.join(__dirname,'./app/public')));
 require("./app/routing/apiRoutes")(app, connection);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log(`Sever running on port`)
 });
