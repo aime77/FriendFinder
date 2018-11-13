053a105018df7ef5c5b4e1a0a6853ec04c8a5739
Jeme <aime.urquieta@outlook.com>
2018-11-12 22:54:32 -0600

    cleaned code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,8 @@
 module.exports = (app, connection) => {
-    //selection all data from mysql dataFriends databse and creating a json object
+    //selecting all data from databse and creating a json object
     app.get("/api/friends", (req, res) => {
         connection.query("SELECT * FROM friends", (err, results) => {
             if (err) throw err;
-            console.log(results);
             let friendsArray = [];
             results.forEach(val => {
                 friendsObj = {
@@ -16,24 +15,13 @@ module.exports = (app, connection) => {
             return res.json(friendsArray);
         })
     })
-    
-    app.post("/api/friends", (req, response, next) => {
+
+    app.post("/api/friends", (req, response) => {
         let newFriend = req.body;
         let totalNumArray = [];
         let selectedFriendObj;
         let newScores = newFriend.scores.join(',');
 
-        //add new friend to database
-        connection.query(`INSERT INTO friends SET ?`,
-            {
-                name: newFriend.name,
-                photo: newFriend.photo,
-                scores: newScores,
-            },
-            (err, results) => {
-                if (err) throw err;
-                console.log(results);
-            });
         //calculations to find best match
         connection.query("SELECT * FROM friends", (err, res) => {
             if (err) throw err;
@@ -44,13 +32,9 @@ module.exports = (app, connection) => {
                 for (j = 0; j < newFriend.scores.length; j++) {
                     totalNum += Math.abs(parseInt(res[i].scores.split(',')[j]) - parseInt(newFriend.scores[j]));
                 }
-                console.log(totalNum);
                 totalNumArray.push(totalNum);
             }
-            console.log(`Array ${totalNumArray}`);
-            //var newIndex=totalNumArray.findIndex(Math.min(...totalNumArray));
             const selectedFriend = Math.min(...totalNumArray);
-
             const newIndex = totalNumArray.indexOf(selectedFriend);
 
             selectedFriendObj = {
@@ -60,6 +44,15 @@ module.exports = (app, connection) => {
             response.send(selectedFriendObj);
         });
 
-
+         //add new friend to database
+         connection.query(`INSERT INTO friends SET ?`,
+         {
+             name: newFriend.name,
+             photo: newFriend.photo,
+             scores: newScores,
+         },
+         (err) => {
+             if (err) throw err;
+         });
     })
 }
